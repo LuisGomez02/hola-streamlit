bb47d17cd7f3c698dc9f042423b78cc757609715
[LuisGomez02] <[Luiscarlosgomez2000@gmail.com]>
2022-11-21 11:57:56 -0600

    agregar datos

---
+++ main.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.title("Hola mundo")
+
+st.header("Primer encabezado")
+
+st.subheader("Primer subencabezado")
+
+st.write("Texto bajo encabezado")
+
+st.header("Segundo encabezado")
+
+st.subheader("Segundo subencabezado")
+
+st.write("Texto bajo segundo ancabezado")
+
+st.markdown("_Itálica_, __negrita__ y [UCR](https://www.ucr.ac.cr/)")
